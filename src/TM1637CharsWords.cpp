c6735e4753fdf2b3170d9bc0896c424e47bf1098
ash <ash@asboy2035.com>
2025-06-02 20:10:14 +0200

    code improvements

--- src/TM1637CharsWords.cpp
+++ src/TM1637CharsWords.cpp
@@ -3,21 +3,23 @@
 #include <TM1637CharsWords.h>
 
 // Loader positions
-const byte L1 = SEG_A;
-const byte L2 = SEG_B;
-const byte L3 = SEG_C;
-const byte L4 = SEG_D;
-const byte L5 = SEG_E;
-const byte L6 = SEG_F;
+constexpr byte L1 = SEG_A;
+constexpr byte L2 = SEG_B;
+constexpr byte L3 = SEG_C;
+constexpr byte L4 = SEG_D;
+constexpr byte L5 = SEG_E;
+constexpr byte L6 = SEG_F;
+
 // Loader positions on all 4 digits
-const byte LOADER[] = {
+constexpr byte LOADER[] = {
   L1, L2, L3, L4, L5, L6
 };
-const int LOADER_LENGTH = sizeof(LOADER) / sizeof(LOADER[0]);
+
+constexpr int LOADER_LENGTH = sizeof(LOADER) / sizeof(LOADER[0]);
 
 // Functions
 void delaySeconds(
-  int seconds
+  const int seconds
 ) {
   delay(seconds * 1000); // in MS
 }
@@ -25,8 +27,8 @@ void delaySeconds(
 void displayArray(
   TM1637Display display,
   byte array[],
-  int arrayLength,
-  int itemDelay = 1
+  const int arrayLength,
+  const int itemDelay
 ) {
   for (int i = 0; i < arrayLength; i+=4) {
     display.setSegments(&array[i], 4, 0);
@@ -36,29 +38,27 @@ void displayArray(
 
 void displayLoader(
   TM1637Display display,
-  unsigned int position
+  const unsigned int position
 ) {
-    for (int i = 0; i < LOADER_LENGTH; i++) {
-    display.setSegments(&LOADER[i], 1, position);
+  for (const unsigned char & i : LOADER) {
+    display.setSegments(&i, 1, position);
     delay(100);
   }
 }
 
 void playLoaderSequence(
-  TM1637Display display,
-  unsigned int position,
-  unsigned int times
+  const TM1637Display display,
+  const unsigned int position,
+  const unsigned int times
 ) {
-  for (int i = 0; i < times; i++)
-  {
+  for (unsigned int i = 0; i < times; i++) {
     displayLoader(display, position);
   }
-  
 }
 
 void displayAlphabet(
-  TM1637Display display,
-  int letterDelay = 1
+  const TM1637Display display,
+  const int letterDelay
 ) {
-  displayArray(display, (byte *)LETTERS, NUM_LETTERS, letterDelay);
+  displayArray(display, const_cast<byte*>(LETTERS), NUM_LETTERS, letterDelay);
 }
--- src/TM1637CharsWords.h
+++ src/TM1637CharsWords.h
@@ -5,55 +5,55 @@
 #include <TM1637Display.h>
 
 // Numbers
-const byte n0 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
-const byte n1 = SEG_B | SEG_C;
-const byte n2 = SEG_A | SEG_B | SEG_D | SEG_E | SEG_G;
-const byte n3 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_G;
-const byte n4 = SEG_B | SEG_C | SEG_F | SEG_G;
-const byte n5 = SEG_A | SEG_C | SEG_D | SEG_F | SEG_G;
-const byte n6 = SEG_A | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
-const byte n7 = SEG_A | SEG_B | SEG_C;
-const byte n8 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
-const byte n9 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G;
+constexpr byte n0 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
+constexpr byte n1 = SEG_B | SEG_C;
+constexpr byte n2 = SEG_A | SEG_B | SEG_D | SEG_E | SEG_G;
+constexpr byte n3 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_G;
+constexpr byte n4 = SEG_B | SEG_C | SEG_F | SEG_G;
+constexpr byte n5 = SEG_A | SEG_C | SEG_D | SEG_F | SEG_G;
+constexpr byte n6 = SEG_A | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
+constexpr byte n7 = SEG_A | SEG_B | SEG_C;
+constexpr byte n8 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
+constexpr byte n9 = SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G;
 
 // Symbols
-const byte _ = SEG_D;
-const byte dash = SEG_G;
+constexpr byte underscore = SEG_D;
+constexpr byte dash = SEG_G;
 
 // Letters
-const byte A = SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
-const byte b = SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
-const byte C = SEG_A | SEG_D | SEG_E | SEG_F;
-const byte d = SEG_B | SEG_C | SEG_D | SEG_E | SEG_G;
-const byte E = SEG_A | SEG_D | SEG_E | SEG_F | SEG_G;
-const byte F = SEG_A | SEG_E | SEG_F | SEG_G;
-const byte G = SEG_A | SEG_C | SEG_D | SEG_E | SEG_F;
-const byte H = SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
-const byte I = SEG_B | SEG_C;
-const byte J = SEG_B | SEG_C | SEG_D | SEG_E;
-const byte L = SEG_D | SEG_E | SEG_F;
-const byte n = SEG_C | SEG_E | SEG_G;
-const byte O = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
-const byte P = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;
-const byte q = SEG_A | SEG_B | SEG_C | SEG_F | SEG_G;
-const byte r = SEG_E | SEG_G;
-const byte S = SEG_A | SEG_C | SEG_D | SEG_F | SEG_G;
-const byte t = SEG_D | SEG_E | SEG_F | SEG_G;
-const byte U = SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
-const byte Y = SEG_B | SEG_C | SEG_D | SEG_F | SEG_G;
-const byte X = SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
-const byte Z = SEG_A | SEG_B | SEG_D | SEG_E | SEG_G;
+constexpr byte A = SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
+constexpr byte b = SEG_C | SEG_D | SEG_E | SEG_F | SEG_G;
+constexpr byte C = SEG_A | SEG_D | SEG_E | SEG_F;
+constexpr byte d = SEG_B | SEG_C | SEG_D | SEG_E | SEG_G;
+constexpr byte E = SEG_A | SEG_D | SEG_E | SEG_F | SEG_G;
+constexpr byte F = SEG_A | SEG_E | SEG_F | SEG_G;
+constexpr byte G = SEG_A | SEG_C | SEG_D | SEG_E | SEG_F;
+constexpr byte H = SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
+constexpr byte I = SEG_B | SEG_C;
+constexpr byte J = SEG_B | SEG_C | SEG_D | SEG_E;
+constexpr byte L = SEG_D | SEG_E | SEG_F;
+constexpr byte n = SEG_C | SEG_E | SEG_G;
+constexpr byte O = SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
+constexpr byte P = SEG_A | SEG_B | SEG_E | SEG_F | SEG_G;
+constexpr byte q = SEG_A | SEG_B | SEG_C | SEG_F | SEG_G;
+constexpr byte r = SEG_E | SEG_G;
+constexpr byte S = SEG_A | SEG_C | SEG_D | SEG_F | SEG_G;
+constexpr byte t = SEG_D | SEG_E | SEG_F | SEG_G;
+constexpr byte U = SEG_B | SEG_C | SEG_D | SEG_E | SEG_F;
+constexpr byte Y = SEG_B | SEG_C | SEG_D | SEG_F | SEG_G;
+constexpr byte X = SEG_B | SEG_C | SEG_E | SEG_F | SEG_G;
+constexpr byte Z = SEG_A | SEG_B | SEG_D | SEG_E | SEG_G;
 
 // Strings
-const byte helo[] = { H, E, L, O };
-const byte good[] = { G, O, O, d };
-const byte stop[] = { S, t, O, P };
-const byte hold[] = { H, O, L, d };
-const byte exit1[] = { E, X, I, t };
-const byte done[] = { d, O, n, E };
-const byte nope[] = { n, O, P, E };
-const byte all_on[] = { n8, n8, n8, n8 };
-const byte LETTERS[] = {
+constexpr byte helo[] = { H, E, L, O };
+constexpr byte good[] = { G, O, O, d };
+constexpr byte stop[] = { S, t, O, P };
+constexpr byte hold[] = { H, O, L, d };
+constexpr byte exit1[] = { E, X, I, t };
+constexpr byte done[] = { d, O, n, E };
+constexpr byte nope[] = { n, O, P, E };
+constexpr byte all_on[] = { n8, n8, n8, n8 };
+constexpr byte LETTERS[] = {
     A,  // Index 0
     b,  // Index 1
     C,  // Index 2
@@ -76,9 +76,9 @@ const byte LETTERS[] = {
     Y,  // Index 19
     Z   // Index 20
 };
-const int NUM_LETTERS = sizeof(LETTERS) / sizeof(LETTERS[0]);
+constexpr int NUM_LETTERS = sizeof(LETTERS) / sizeof(LETTERS[0]);
 
-const byte NUMBERS[] = {
+constexpr byte NUMBERS[] = {
     n0,  // Index 0
     n1,  // Index 1
     n2,  // Index 2
@@ -88,9 +88,9 @@ const byte NUMBERS[] = {
     n6,  // Index 6
     n7,  // Index 7
     n8,  // Index 8
-    n9  // Index 9
+    n9   // Index 9
 };
-const int NUM_NUMBERS = sizeof(NUMBERS) / sizeof(NUMBERS[0]);
+constexpr int NUM_NUMBERS = sizeof(NUMBERS) / sizeof(NUMBERS[0]);
 
 extern void displayArray(
   TM1637Display display,
